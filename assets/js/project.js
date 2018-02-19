617bf5a700768f357bae48e8f4403dfdc98f5586
brad stahler <bradstahler@brads-MacBook-Air.local>
2018-02-18 18:00:07 -0600

    edits

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,6 +1,44 @@
 
-$("#button").click(function() {
+$(".services").click(function() {
+	event.preventDefault() 
+	$(".contact").removeClass("active");
+	$(".services").addClass("active");
+	$(".gallery").removeClass("active");
+
     $('html, body').animate({
         scrollTop: $("#section1").offset().top
+    }, 1500);
+});
+
+$(".contact").click(function() {
+	event.preventDefault() 
+	$(".contact").addClass("active");
+	$(".services").removeClass("active");
+	$(".gallery").removeClass("active");
+
+    $('html, body').animate({
+        scrollTop: $("#contact-container").offset().top
+    }, 1500);
+});
+
+$(".gallery").click(function() {
+	event.preventDefault() 
+	$(".contact").removeClass("active");
+	$(".services").removeClass("active");
+	$(".gallery").addClass("active");
+
+    $('html, body').animate({
+        scrollTop: $("#gallery").offset().top
+    }, 1500);
+});
+$(".home").click(function() {
+	event.preventDefault() 
+	$(".home").addClass("active");
+	$(".contact").removeClass("active");
+	$(".services").removeClass("active");
+	$(".gallery").removeClass("active");
+
+    $('html, body').animate({
+        scrollTop: $("#carousel-example-generic").offset().top
     }, 1500);
 });
